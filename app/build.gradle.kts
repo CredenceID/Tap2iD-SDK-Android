fc4afc7867739e829868545934fa2dbf0b27d3d4
ankitgh <ankit.ghate@credenceid.com>
2024-08-23 17:43:35 +0530

    [BUILD] Update Sample app version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.0-RC3"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
